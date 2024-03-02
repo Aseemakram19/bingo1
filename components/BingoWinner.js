765c967b87522c93a95ca51b6a4797b5e07afd61
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2024-03-02 23:50:43 +0530

    Delete components directory

--- components/BingoWinner.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/BingoWinner.module.css";
-
-export default function BingoWinner(props) {
-  return (
-    <section className={styles.main}>
-      <p className={styles.title}>{props.winner} victory!</p>
-    </section>
-  );
-}
